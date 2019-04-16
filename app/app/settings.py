801e27b0780c4a1cad35d7384ea9518843169359
maks-prost <zendzin@list.ru>
2019-04-16 10:08:35 +0500

    using mysql

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'app',                  
+        'USER': 'django',             
+        'PASSWORD': '1122',                  
+        'HOST': 'localhost',                      
     }
 }
 
